e322dfbabf9c25c9aab835704a565c2882ca83ff
lixiaoguang <lixiaoguang@xiaoniu66.com>
2019-02-15 15:53:22 +0800

    cmt: add WebRTC pod

---
+++ WebRTCCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebRTCCheck
+//
+//  Created by xiaoniu on 2019/2/15.
+//  Copyright © 2019 xiaoguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebRTCCheck/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  WebRTCCheck
+//
+//  Created by xiaoniu on 2019/2/15.
+//  Copyright © 2019 xiaoguang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
